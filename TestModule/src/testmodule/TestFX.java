2c366d98b801c215397d0d625cf4fdc8a6ec590a
Joker <35430758+Joker6493@users.noreply.github.com>
2018-01-29 11:04:50 +0400

    Add files via upload

---
+++ TestModule/src/testmodule/TestFX.java
@@ -0,0 +1,54 @@
+package testmodule;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import api.NexusPlugin;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author d.borodin
+ */
+public class TestFX extends Application implements NexusPlugin {
+    
+
+    @Override
+    public void start(Stage myStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        myStage.setTitle("Hello World!");
+        myStage.setScene(scene);
+        myStage.show();
+    }
+
+    @Override
+    public void invoke() {
+        System.out.println("Hello World!");
+        this.launch();
+        
+    }
+   
+}
