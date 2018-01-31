53822d289142c8ab718203a77a22e946fefd6215
Joker <d.borodin@spt-5.farmnet.local>
2018-01-31 21:56:46 +0400

    check

---
+++ NexusFX/src/oracledbconn/OracleConn.java
@@ -0,0 +1,81 @@
+<<<<<<< HEAD
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oracledbconn;
+
+import java.sql.*;
+
+/**
+ *
+ * @author dboro
+ */
+public class OracleConn {
+    private final String url = "jdbc:oracle:thin:@localhost:1521:XE";
+    private final String login = "nexusdb";
+    private final String password = "Stavr0p0l";    
+    public void connectDatabase() {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con = DriverManager.getConnection(url, login, password);
+        } catch (Exception e){
+            System.out.println("Ошибка " + e.getMessage());
+            e.printStackTrace();
+        }
+        /*            try {
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM SYSTEM_INFO");
+        System.out.println("Результат запроса");
+        System.out.println("ID Систем и Код: ");
+        while (rs.next()) {
+        String str = rs.getString("systemid") + ":" + rs.getString(2);
+        System.out.println("ID: " + str);
+        }
+        rs.close();
+        stmt.close();
+        } finally {
+        con.close();
+        System.out.println("Соединение закрыто");
+        }
+        } catch (Exception e) {
+        System.out.println("Ошибка " + e.getMessage());
+        e.printStackTrace();*/
+    }
+}
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oracledbconn;
+
+/**
+ *
+ * @author dboro
+ */
+import java.sql.*;
+
+
+
+public class OracleConn {
+    
+    public String url = "jdbc:oracle:thin:@localhost:1521:XE";
+    public String login = "nexusdb";
+    public String password = "Stavr0p0l";
+    
+    public void connectDatabase(){
+       try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con = DriverManager.getConnection(url, login, password);
+            System.out.println("Соединение установлено");
+        } catch (Exception e) {
+            System.out.println("Ошибка " + e.getMessage());
+            e.printStackTrace();
+        }
+       
+    }
+}
+>>>>>>> 17e063a6591b5bfa7eeeca47c78ecf7e6e4c7d66
