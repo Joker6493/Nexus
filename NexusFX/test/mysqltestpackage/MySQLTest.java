d4c886c1d70a06d9512188fdcc3811b1c9f37c9c
d.borodin <d.borodin@spt-5.farmnet.local>
2018-04-03 17:11:02 +0400

    upd

--- NexusFX/test/mysqltestpackage/MySQLTest.java
+++ NexusFX/test/mysqltestpackage/MySQLTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package oracletestpackage;
+package mysqltestpackage;
 
 import java.sql.*;
 import java.time.LocalDate;
@@ -52,7 +52,6 @@ public class MySQLTest {
             }
         } catch (Exception e) {
             System.out.println("Ошибка " + e.getMessage());
-            e.printStackTrace();
         }
     }
     
